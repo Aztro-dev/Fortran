b20463aacc06b56cc24679e91c91d36792ebbeb0
Aztro-dev <davidperezcastellanos@gmail.com>
2025-09-16 13:10:10 -0500

    Color and cursor

--- Minesweeper/board.f90
+++ Minesweeper/board.f90
@@ -20,14 +20,20 @@ contains
       ! * for blank/not clicked
       ! _ (space) for nothing there
       ! 1-8 for bomb numbers
-      ! # for bomb
+      ! F for flag
       do i = 1, n
          do j = 1, 2*n
             visible_board(i, j) = ' '
          end do
       end do
 
-      visible_board(2, 2) = '#'
+      ! TODO make non-hardcoded
+      ! Hardcoded values for testing
+      visible_board(2, 2) = 'F'
+      visible_board(2, 3) = ' '
+      visible_board(2, 4) = '3'
+      visible_board(2, 5) = '*'
+      cursor = (3.0, 2.0)
 
       ! We will not populate the bomb board until the user has selected a spot.
       allocate (bomb_board(n, 2*n))
@@ -46,18 +52,29 @@ contains
          ! Write all characters/cells in the line
          do j = 1, 2*n
             ! Don't advance so we don't create a ton of newlines
+
+            ! If the cursor is currently on the cell, we gray it out a lil bit
+            if (int(cursor%RE) .EQ. i .AND. int(cursor%IM) .eq. j) then
+               if (visible_board(i, j) .EQ. 'F') then
+                  call print_colored(BG_CYAN, FG_BRIGHT_RED, visible_board(i, j))
+               else
+                  call print_colored(BG_CYAN, FG_BLACK, visible_board(i, j))
+               end if
+               cycle
+            end if
+
             ! * for blank/not clicked
             ! _ (space) for nothing there
             ! 1-8 for bomb numbers
-            ! # for bomb
-            if (visible_board(i, j) .EQ. '#') then
-               call print_colored(BG_RED, FG_RED, ' ')
+            ! F for flag
+            if (visible_board(i, j) .EQ. 'F') then
+               call print_colored(BG_RED, FG_BLACK, 'F')
             else if (visible_board(i, j) .EQ. '*') then
-               call print_colored(BG_WHITE, FG_BLACK, '*')
+               call print_colored(BG_BRIGHT_BLACK, FG_BLACK, '*')
             else if (visible_board(i, j) .EQ. ' ') then
-               call print_colored(BG_WHITE, FG_BLACK, '$')
+               call print_colored(BG_WHITE, FG_BLACK, ' ')
             else
-               write (*, '(A, "[47m", " ", A, "[0m")', advance="no") color_escape, color_escape
+               call print_colored(BG_BRIGHT_BLACK, FG_WHITE, visible_board(i, j))
             end if
          end do
          ! newline
